46b19fe2ca850fdf6d69d7b279a0ee3b75e60509
Lucas Montanari <lmontanari@csumb.edu>
2018-03-03 02:34:24 +0000

    lab4

--- Labs/Lab4/api/pixabayAPI.php
+++ Labs/Lab4/api/pixabayAPI.php
@@ -1,10 +1,10 @@
 <?php
 
 //returns array with 100 URLs to images from Pixabay.com, based on a "keyword"
-function getImageURLs($keyword, $orientation="horizontal") {
+function getImageURLs($keyword, $orientation) {
     $curl = curl_init();
     curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
+      CURLOPT_URL => "https://pixabay.com/api/?key=8098358-8f9d95dfbb9a3e5df84d8199f&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
@@ -18,6 +18,7 @@ function getImageURLs($keyword, $orientation="horizontal") {
     $data = json_decode($jsonData, true); //true makes it an array!
     
     $imageURLs = array();
+   
     for ($i = 0; $i < 99; $i++) {
     $imageURLs[] = $data['hits'][$i]['webformatURL'];
     }
